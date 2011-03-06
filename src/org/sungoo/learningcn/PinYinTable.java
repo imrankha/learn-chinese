d134ca2924064a850cfc4692ce7a25ce2795d7c2
yongli12 <yongli12@gmail.com>
2011-03-06 20:31:22 +0000

    Remove commented out stuff.

--- src/org/sungoo/learningcn/PinYinTable.java
+++ src/org/sungoo/learningcn/PinYinTable.java
@@ -7,7 +7,7 @@ public class PinYinTable {
 //	public static final String INIT_e = "e";
 //	public static final String INIT_i = "i";
 //	public static final String INIT_u = "u";
-//	public static final String INIT_v = "Ÿ";
+//	public static final String INIT_v = "ÂŸ";
 //	public static final String INIT_ai = "ai";
 //	public static final String INIT_ei = "ei";
 //	public static final String INIT_ui = "ui";
@@ -15,13 +15,13 @@ public class PinYinTable {
 //	public static final String INIT_ou = "ou";
 //	public static final String INIT_iu = "iu";
 //	public static final String INIT_ie = "ie";
-//	public static final String INIT_ve = "Ÿe";
+//	public static final String INIT_ve = "ÂŸe";
 //	public static final String INIT_er = "er";
 //	public static final String INIT_an = "an";
 //	public static final String INIT_en = "en";
 //	public static final String INIT_in = "in";
 //	public static final String INIT_un = "un";
-//	public static final String INIT_vn = "Ÿn";
+//	public static final String INIT_vn = "ÂŸn";
 //	public static final String INIT_ang = "ang";
 //	public static final String INIT_eng = "eng";
 //	public static final String INIT_ing = "ing";
@@ -32,7 +32,7 @@ public class PinYinTable {
 		"e",
 		"i",
 		"u",
-		"Ÿ",
+		"ÂŸ",
 		"ai",
 		"ei",
 		"ui",
@@ -40,43 +40,19 @@ public class PinYinTable {
 		"ou",
 		"iu",
 		"ie",
-		"Ÿe",
+		"ÂŸe",
 		"er",
 		"an",
 		"en",
 		"in",
 		"un",
-		"Ÿn",
+		"ÂŸn",
 		"ang",
 		"eng",
 		"ing",
 		"ong",
 	};
 
-	// Finals
-//	public static final String FINAL_b = "b";
-//	public static final String FINAL_p = "p";
-//	public static final String FINAL_m = "m";
-//	public static final String FINAL_f = "f";
-//	public static final String FINAL_d = "d";
-//	public static final String FINAL_t = "t";
-//	public static final String FINAL_n = "n";
-//	public static final String FINAL_l = "l";	
-//	public static final String FINAL_g = "g";
-//	public static final String FINAL_k = "k";
-//	public static final String FINAL_h = "h";
-//	public static final String FINAL_j = "j";	
-//	public static final String FINAL_q = "q";
-//	public static final String FINAL_x = "x";
-//	public static final String FINAL_zh = "zh";
-//	public static final String FINAL_ch = "ch";
-//	public static final String FINAL_sh = "sh";
-//	public static final String FINAL_r = "r";
-//	public static final String FINAL_z = "z";
-//	public static final String FINAL_c = "c";
-//	public static final String FINAL_s = "s";
-//	public static final String FINAL_y = "y";
-//	public static final String FINAL_w = "w";
 	public static final String[]  initials= {
 		"b",
 		"p",
