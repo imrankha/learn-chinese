bc6ea04b1675ad34f94d6e645ab19f34435f6e19
yongli12 <yongli12@3155d53a-5d67-20ad-8cbd-549b570d124a>
2011-03-06 16:47:15 +0000

    initial import

---
+++ src/org/sungoo/learningcn/LearningCN.java
@@ -0,0 +1,35 @@
+package org.sungoo.learningcn;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class LearningCN extends Activity implements OnClickListener {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        // click listeners
+        View pinyinButton = this.findViewById(R.id.pinyin);
+        pinyinButton.setOnClickListener(this);
+        View hanziButton = this.findViewById(R.id.hanzi);
+        hanziButton.setOnClickListener(this);   
+    }
+    
+    public void onClick(View v) {
+    	switch (v.getId()) {
+    	case R.id.pinyin:
+    		Intent i = new Intent(this, Pinyin.class);
+    		startActivity(i);
+    		break;
+    	case R.id.hanzi:
+    		Intent i2 = new Intent(this, Hanzi.class);
+    		startActivity(i2);
+    		break;    	
+    	}
+    }
+}
