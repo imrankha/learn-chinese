5414027f7e299fc810fd873e632b0c6af9c16e8b
shubin <shubin@3155d53a-5d67-20ad-8cbd-549b570d124a>
2012-10-07 00:30:34 +0000

    Stable version.  - Added a new icon.  - Minor user experience improvements to the MathView.

--- gen/org/sungoo/learningcn/R.java
+++ gen/org/sungoo/learningcn/R.java
@@ -22,7 +22,8 @@ public final class R {
         public static final int title=0x7f050001;
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int app_icon=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int all_pinyin=0x7f08001f;
